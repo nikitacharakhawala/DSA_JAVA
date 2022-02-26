d918f9a4ddb97cf273fb7ac5cd502dd48945a813
nikitacharakhawala <48835295+nikitacharakhawala@users.noreply.github.com>
2022-02-26 22:30:22 +0530

    changed code

--- Java Beginner/FullPyramidPattern.java
+++ Java Beginner/FullPyramidPattern.java
@@ -18,19 +18,23 @@ public class FullPyramidPattern {
         int value=1;
         for(int i=1;i<=n;i++){
             value=i;
+            //Initial 0's loop
             for(int count=1;count<=n-i; count++){
                 System.out.print("0 ");
             }
+            //Middle part Number
             for(int j=1;j<=i;j++){
                 System.out.print(value+" ");
                 value++;
 
             }
+            //Right part where numbers are decreasing
             value=value-2;
             for(int j=1;j<i;j++){
                 System.out.print(value+" ");
                 value--;
             }
+            //Last part 0's
             for(int count=n-i;count>=1;count--){
                 System.out.print("0 ");
             }
