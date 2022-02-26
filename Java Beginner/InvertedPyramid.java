be41a620534eb40cf069d33456ab8f8f438151a3
nikitacharakhawala <48835295+nikitacharakhawala@users.noreply.github.com>
2022-02-26 22:37:36 +0530

    added inverted pyramid

---
+++ Java Beginner/InvertedPyramid.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(j+1);
+                if(j!=(i-1)){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
