02e6ae10f2c381d4a8e953fa7b71d3d8e6a262c6
nikitacharakhawala <48835295+nikitacharakhawala@users.noreply.github.com>
2022-03-12 17:33:51 +0530

    added perfect number

---
+++ javadsa/mathsproblems/PerfectNumber.java
@@ -0,0 +1,30 @@
+package javadsa.mathsproblems;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printPerfectNumber(n);
+    }
+
+    public static void printPerfectNumber(int n){
+        for(int i=2;i<n;i++){
+            int sum=1;
+           for(int num=2;num*num<=i;num++){
+               if(i%num==0){
+                   if(num*num!=i){
+                       sum=sum+num+i/num;
+                   }else{
+                       sum=sum+num;
+                   }
+               }
+           }
+           if(sum==i && i!=1){
+               System.out.println(i);
+           }
+
+        }
+    }
+}
