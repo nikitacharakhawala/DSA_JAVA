47ab9761136da4ace3bdc760e0351f0caf0f2992
nikitacharakhawala <48835295+nikitacharakhawala@users.noreply.github.com>
2022-05-21 22:01:21 +0530

    new code

---
+++ javadsaintermediate/leetcode/TwoSum.java
@@ -0,0 +1,79 @@
+package javadsaintermediate.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 1. Two Sum
+ * Easy
+ *
+ * 32337
+ *
+ * 1027
+ *
+ * Add to List
+ *
+ * Share
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ *
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ *
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ *
+ *
+ * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] A={2,4,11,3};
+        int target=6;
+        System.out.println(Arrays.toString(getTwoSum(A,target)));
+    }
+
+    public static int[] getTwoSum(int[] A, int target){
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        int[] answer=new int[2];
+        for(int i=0;i<A.length;i++){
+            if(!hs.containsKey(A[i])){
+                hs.put(A[i], i);
+            }
+        }
+        for(int i=0;i<A.length;i++){
+            int x=target-A[i];
+            if(hs.containsKey(x)){
+                if(hs.get(x)!=i){
+                    answer[0]=hs.get(x);
+                    answer[1]=i;
+                }
+
+
+            }
+        }
+        return answer;
+    }
+}
---
+++ javadsaintermediate/tree/PreOrderTraversal.java
@@ -0,0 +1,125 @@
+package javadsaintermediate.tree;
+/**
+ * Q2. Preorder Traversal
+ * Solved
+ * character backgroundcharacter
+ * Stuck somewhere?
+ * Ask for help from a TA and get it resolved.
+ * Get help from TA.
+ *
+ * Problem Description
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ *
+ * NOTE: Using recursion is not allowed.
+ *
+ *
+ *
+ * Problem Constraints
+ *
+ * 1 <= number of nodes <= 105
+ *
+ *
+ *
+ * Input Format
+ *
+ * First and only argument is root node of the binary tree, A.
+ *
+ *
+ *
+ * Output Format
+ *
+ * Return an integer array denoting the preorder traversal of the given binary tree.
+ *
+ *
+ *
+ * Example Input
+ *
+ * Input 1:
+ *
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Input 2:
+ *
+ *    1
+ *   / \
+ *  6   2
+ *     /
+ *    3
+ *
+ *
+ *
+ * Example Output
+ *
+ * Output 1:
+ *
+ *  [1, 2, 3]
+ *
+ * Output 2:
+ *
+ *  [1, 6, 2, 3]
+ *
+ *
+ *
+ * Example Explanation
+ *
+ * Explanation 1:
+ *
+ *  The Preoder Traversal of the given tree is [1, 2, 3].
+ *
+ * Explanation 2:
+ *
+ *  The Preoder Traversal of the given tree is [1, 6, 2, 3].
+ */
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+        left=null;
+        right=null;
+    }
+}
+
+public class PreOrderTraversal {
+    public static void main(String[] args) {
+        TreeNode root=new TreeNode(1);
+        TreeNode lr=new TreeNode(2);
+        root.left=lr;
+        TreeNode lr1=new TreeNode(4);
+        lr.left=lr1;
+        TreeNode lr2=new TreeNode(5);
+        lr.right=lr2;
+        TreeNode rl=new TreeNode(3);
+        root.right=rl;
+        TreeNode rl1=new TreeNode(6);
+        rl.left=rl1;
+        TreeNode rl2=new TreeNode(7);
+        rl.right=rl2;
+        ArrayList<Integer> ans=new ArrayList<>();
+        ans=preOrderTraversal(root, ans);
+        for(Integer ele: ans){
+            System.out.print(ele+" ");
+        }
+    }
+
+    public static ArrayList<Integer> preOrderTraversal(TreeNode root, ArrayList<Integer> A){
+           if(root==null)
+               return A;
+           A.add(root.val);
+           preOrderTraversal(root.left, A);
+           preOrderTraversal(root.right, A);
+           return A;
+    }
+}
